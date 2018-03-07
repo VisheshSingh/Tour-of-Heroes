105ad9fe0a3cf8ff0340fe09938ee889db507c59
Vishesh <visheshsingh16@gmail.com>
2018-03-06 21:41:43 -0600

    Service-in-Service for showing messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,15 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private _ms: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this._ms.add('HeroService: fetched heroes');
     return of(HEROES);
   }
   
